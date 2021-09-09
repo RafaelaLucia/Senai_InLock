4f17996095fa6ee1b57dea6e8bb14c2b1b4d152d
Rafaela Lucia <rafaluciadev@gmail.com>
2021-09-09 18:45:16 -0300

    usuarios

--- Backend/senai.inlock.webApi/Domains/UsuarioDomain.cs
+++ Backend/senai.inlock.webApi/Domains/UsuarioDomain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,11 @@ namespace senai.inlock.webApi_.Domains
     {
         public int idUsuario { get; set; }
         public int idTipoUsuario { get; set; }
+
+        [Required(ErrorMessage = "Informe o email")]
         public string email { get; set; }
+        [Required(ErrorMessage = "Informe a senha")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "O campo senha precisa ter no mínimo 3 e no máximo 10 caracteres")]
         public string senha { get; set; }
         public TipoUsuarioDomain tipoUsuario { get; set; }
     }
--- Backend/senai.inlock.webApi/Startup.cs
+++ Backend/senai.inlock.webApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,33 @@ namespace senai.inlock.webApi_
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+
+            })
+                 .AddJwtBearer("JwtBearer", options => {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         //validar quem esta emitindo o token
+                         ValidateIssuer = true,
+                         //validar quem esta recebendo o token
+                         ValidateAudience = true,
+                         //validar o tempo de expiracao
+                         ValidateLifetime = true,
+                         //definindo a chave de seguranca
+                         IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("inlock-chave-autenticacao")),
+                         //tempo de expiracao do token
+                         ClockSkew = TimeSpan.FromMinutes(30),
+                         //define o nome de quem emite o token (issue)
+                         ValidIssuer = "inlock.webApi",
+                         //define o nome de quem recebe o token (audience)
+                         ValidAudience = "inlock.webApi"
+                     };
+                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +55,12 @@ namespace senai.inlock.webApi_
             }
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
